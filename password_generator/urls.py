17fac3744240d9c565362622e68809085722161c
rauulssanchezz <rauulssanchezz@gmail.com>
2025-08-28 19:28:48 +0200

    update urls

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -1,5 +1,13 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
+from passwords.views import signup, save_password
 
 urlpatterns = [
-    path('', include('passwords.urls')),
+    path('', include('passwords.urls'), name='password'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('admin/', admin.site.urls),
+    path('signup/', signup, name='signup'),
+    path('save-password', save_password, name='save-password')
 ]
